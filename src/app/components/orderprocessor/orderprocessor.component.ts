e186534771ae721caa276c960ff375ddec1adf8a
lutfiqaraman <lutfiqaraman@gmail.com>
2022-07-19 22:49:01 +0300

    use input properties

--- src/app/components/orderprocessor/orderprocessor.component.ts
+++ src/app/components/orderprocessor/orderprocessor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-orderprocessor',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orderprocessor.component.css']
 })
 export class OrderprocessorComponent implements OnInit {
+  @Input() stockSymbol: string = '';
+  @Input() quantity: number = 0;
 
   constructor() { }
 
